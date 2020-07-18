72fba8107abcba0d682767b5fdc3f557bd33bd57
alesavin <alesavin@gmail.com>
2020-07-18 20:05:59 +0200

    demodulate based on cons, support for tuples

--- src/main/java/icfpc2020/DemodulateList.java
+++ src/main/java/icfpc2020/DemodulateList.java
@@ -3,56 +3,28 @@ package icfpc2020;
 import icfpc2020.operators.Demodulate;
 import icfpc2020.operators.Modulate;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-import java.util.stream.Stream;
 
 public class DemodulateList {
-
-    private enum State {
-        EMPTY,
-        OPEN,
-        CLOSED,
-        COMMA,
-        NUMBER
-    }
 
     public static List<Pictogram> dem(Message m) {
         String s = m.toString();
         List<Pictogram> result = new ArrayList<>();
-        State state = State.EMPTY;
         while (!s.isEmpty()) {
             if (s.startsWith("11")) {
-                switch (state) {
-                    case EMPTY, COMMA, OPEN -> {
-                        result.add(new CommandR(Command.ListLPar));
-                        state = State.OPEN;
-                    }
-                    default -> {
-                        result.add(new CommandR(Command.ListComma));
-                        state = State.COMMA;
-                    }
-                }
+                result.add(new CommandR(Command.App));
+                result.add(new CommandR(Command.App));
+                result.add(new CommandR(Command.Cons));
                 s = s.substring(2);
             } else if (s.startsWith("00")) {
-                switch (state) {
-                    case EMPTY, OPEN -> result.add(new CommandR(Command.Nil));
-                    default -> result.add(new CommandR(Command.ListRPar));
-                }
-                state = State.CLOSED;
+                result.add(new CommandR(Command.Nil));
                 s = s.substring(2);
             } else {
-                if (state == State.CLOSED) {
-                    result.add(new CommandR(Command.ListComma));
-                }
                 var bi = new Demodulate(s).dem();
                 result.add(new NumberR(bi, Board.EMPTY));
                 var modulateLength = new Modulate(bi).mod().length();
                 s = s.substring(modulateLength);
-                state = State.NUMBER;
             }
         }
         return result;
--- src/test/java/icfpc2020/DemodulateListTest.java
+++ src/test/java/icfpc2020/DemodulateListTest.java
@@ -17,21 +17,23 @@ public class DemodulateListTest {
     public void testModulateList_shouldProduceExpectedResult() {
         Assert.assertEquals("nil",
                 stringify(DemodulateList.dem(new MessageImpl("00"))));
-        Assert.assertEquals("( nil )",
+        Assert.assertEquals("ap ap cons nil nil",
                 stringify(DemodulateList.dem(new MessageImpl("110000"))));
-        Assert.assertEquals("( 0 )",
+        Assert.assertEquals("ap ap cons 0 nil",
                 stringify(DemodulateList.dem(new MessageImpl("1101000"))));
-        Assert.assertEquals("( 1 , 2 )",
+        Assert.assertEquals("ap ap cons 1 2",
+                stringify(DemodulateList.dem(new MessageImpl("110110000101100010"))));
+        Assert.assertEquals("ap ap cons 1 ap ap cons 2 nil",
                 stringify(DemodulateList.dem(new MessageImpl("1101100001110110001000"))));
-        Assert.assertEquals("( 1 , ( 2 , 3 ) , 4 )",
+        Assert.assertEquals("ap ap cons 1 ap ap cons ap ap cons 2 ap ap cons 3 nil ap ap cons 4 nil",
                 stringify(DemodulateList.dem(new MessageImpl("1101100001111101100010110110001100110110010000"))));
-        Assert.assertEquals("( 1 , 0 )",
+        Assert.assertEquals("ap ap cons 1 ap ap cons 0 nil",
                 stringify(DemodulateList.dem(new MessageImpl("11011000011101000"))));
-        Assert.assertEquals("( ( ( ( ( ( ( ( ( ( ( ( ( ( ( (",
+        Assert.assertEquals("ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons ap ap cons",
                 stringify(DemodulateList.dem(new MessageImpl("11111111111111111111111111111111"))));
-        Assert.assertEquals("( 1 , 81744 )",
+        Assert.assertEquals("ap ap cons 1 ap ap cons 81744 nil",
                 stringify(DemodulateList.dem(new MessageImpl("110110000111011111100001001111110101000000"))));
-        Assert.assertEquals("( 1 , 81740 )",
+        Assert.assertEquals("ap ap cons 1 ap ap cons 81740 nil",
                 stringify(DemodulateList.dem(new MessageImpl("110110000111011111100001001111110100110000"))));
     }
 }
