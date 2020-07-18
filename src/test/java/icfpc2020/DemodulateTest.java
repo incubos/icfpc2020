57d3b67e3ca0686f34fee30ca4c3b0883d7fa64e
alesavin <alesavin@gmail.com>
2020-07-18 19:16:44 +0200

    demodulate modulated messages, demodulate BigDecimal

--- src/test/java/icfpc2020/DemodulateTest.java
+++ src/test/java/icfpc2020/DemodulateTest.java
@@ -6,21 +6,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DemodulateTest {
     @Test
     public void test() {
-        Assert.assertEquals(0L, new Demodulate("010").dem());
-        List<BigInteger> cases = Arrays.asList(1L, 256L, -1L, -256L, (long)Integer.MAX_VALUE,
-                                               (long)Integer.MIN_VALUE).stream().map(BigInteger::valueOf)
+        Assert.assertEquals(BigInteger.ZERO, new Demodulate("010").dem());
+//        Assert.assertEquals(BigInteger.ZERO, new Demodulate("0111111000010011111101010000").dem());
+        List<BigInteger> cases = Stream.of(1L, 256L, -1L, -256L, Long.MAX_VALUE,
+                                               Long.MIN_VALUE).map(BigInteger::valueOf)
                 .collect(Collectors.toList());
         for (BigInteger ca: cases) {
             String s = new Modulate(ca).mod();
             System.out.println(ca + " = [" + s + "]");
-            Assert.assertEquals(ca.longValue(), new Demodulate(s).dem());
+            Assert.assertEquals(ca, new Demodulate(s).dem());
         }
     }
 }
