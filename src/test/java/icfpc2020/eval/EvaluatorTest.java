9aff2de685ded93e6eeb58648cb16a07a84a26b8
Vadim Tsesko <incubos@users.noreply.github.com>
2020-07-18 15:01:52 +0300

    Added basic inc eval tests

---
+++ src/test/java/icfpc2020/eval/EvaluatorTest.java
@@ -0,0 +1,38 @@
+package icfpc2020.eval;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author incubos
+ */
+public class EvaluatorTest {
+    private static void eval(
+            @NotNull final String code,
+            @NotNull final String expectedResult) throws Exception {
+        final Evaluator evaluator =
+                new Evaluator(
+                        new ByteArrayInputStream(
+                                code.getBytes(StandardCharsets.UTF_8)));
+        assertEquals(
+                expectedResult,
+                evaluator.evaluate("test").apply().toString());
+    }
+
+    @Test
+    public void inc() throws Exception {
+        eval("test = ap inc 0", "1");
+    }
+
+    @Test
+    public void inc3() throws Exception {
+        eval("test = ap inc ap inc ap inc 0", "3");
+    }
+}
