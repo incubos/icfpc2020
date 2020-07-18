abaa6fd6fbc8dc44b2d1e1e9b55af7f984a8ffed
Anton Volokhov <avolokhov@apple.com>
2020-07-18 15:25:55 +0100

    add rudimentary draw support

---
+++ src/main/java/icfpc2020/Draw.java
@@ -0,0 +1,93 @@
+package icfpc2020;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class Draw {
+    private static final Logger log = LoggerFactory.getLogger(Draw.class);
+
+    private Draw() {
+    }
+
+    private static final Set<Command> ignoreCommands = EnumSet.of(Command.App, Command.Cons,
+                                                                  Command.Nil, Command.ListRPar,
+                                                                  Command.ListLPar,
+                                                                  Command.ListComma);
+
+    private static final Command startToken = Command.Vector;
+
+    public static class Coord {
+        public final long x;
+        public final long y;
+
+        public Coord(final long x, final long y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static Consumer<Coord> drawingBoard = coord -> {
+    };
+
+    private static BigInteger getNumber(Pictogram pictogram) {
+        if (pictogram instanceof NumberR) {
+            return ((NumberR) pictogram).n;
+        } else {
+            throw new UnsupportedOperationException("Draw protocol expects two number after 'vec' command");
+        }
+    }
+
+    public static void draw(List<Pictogram> commands) {
+        var iter = commands.iterator();
+        var index = 0;
+        while (iter.hasNext()) {
+            var pictogram = iter.next();
+            index++;
+            if (pictogram instanceof CommandR) {
+                var command = ((CommandR) pictogram).command;
+                if (ignoreCommands.contains(command)) {
+                    continue;
+                } else if (startToken == command) {
+                    BigInteger x, y;
+                    if (iter.hasNext()) {
+                        x = getNumber(iter.next());
+                        index++;
+                    } else {
+                        log.error("Not enough commands, expected two numbers, commands={}, " +
+                                          "position={}", commands, index);
+                        throw new UnsupportedOperationException("Draw protocol expects at least " +
+                                                                        "two pictograms after 'vec' command");
+                    }
+                    if (iter.hasNext()) {
+                        y = getNumber(iter.next());
+                        index++;
+                    } else {
+                        log.error("Not enough commands, expected two numbers, commands={}, " +
+                                          "position={}", commands, index);
+                        throw new UnsupportedOperationException("Draw protocol expects at least " +
+                                                                        "two pictograms after 'vec' command");
+                    }
+                    drawingBoard.accept(new Coord(x.longValue(), y.longValue()));
+                } else {
+                    log.error("Found unexpected command when drawing, command={}, position={}commands={}",
+                              command,
+                              index,
+                              commands);
+                    throw new UnsupportedOperationException("unexpected command");
+                }
+            } else {
+                log.error("Found unexpected pictogram when drawing, pictogram={}, position={}, commands={}",
+                          pictogram,
+                          index,
+                          commands);
+                throw new UnsupportedOperationException("unexpected pictogram");
+            }
+        }
+    }
+}
