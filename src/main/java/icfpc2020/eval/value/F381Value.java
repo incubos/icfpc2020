21a65ec898235f8416d28f41c937965d735d18d3
Anton Volokhov <avolokhov@apple.com>
2020-07-19 15:20:45 +0100

    add f38

--- src/main/java/icfpc2020/eval/value/F381Value.java
+++ src/main/java/icfpc2020/eval/value/F381Value.java
@@ -4,22 +4,100 @@ import org.jetbrains.annotations.NotNull;
 
 public class F381Value implements LazyValue {
     @NotNull
-    private final LazyValue left;
+    private final LazyValue x2;
 
-    public F381Value(final LazyValue left) {
-        this.left = left;
+    public F381Value(final LazyValue x2) {
+        this.x2 = x2;
     }
 
     @NotNull
     @Override
     public LazyValue apply(@NotNull final LazyValue arg) {
-        // TODO!
-        throw new UnsupportedOperationException();
+        LazyValue x0 = arg;
+        // ap ap ap if0 ap car x0
+        return new ApplyValue(
+                new ApplyValue(
+                        new ApplyValue(
+                                If03Value.INSTANCE,
+                                new ApplyValue(
+                                        CarValue.INSTANCE,
+                                        x0)
+                        ),
+                        // ( ap modem ap car ap cdr x0
+                        new ApplyValue(
+                                new ApplyValue(
+                                        Cons2Value.INSTANCE,
+                                        new ApplyValue(
+                                                ModemValue.INSTANCE,
+                                                new ApplyValue(
+                                                        CarValue.INSTANCE,
+                                                        new ApplyValue(
+                                                                CdrValue.INSTANCE,
+                                                                x0
+                                                        )
+                                                )
+                                        )
+                                ),
+                                // , ap multipledraw ap car ap cdr ap cdr x0 )
+                                new ApplyValue(
+                                        new ApplyValue(
+                                                Cons2Value.INSTANCE,
+                                                new ApplyValue(
+                                                        new LazyValue() {}, // MultipeDraw
+                                                        new ApplyValue(
+                                                                CarValue.INSTANCE,
+                                                                new ApplyValue(
+                                                                        CdrValue.INSTANCE,
+                                                                        new ApplyValue(
+                                                                                CdrValue.INSTANCE,
+                                                                                x0
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        ),
+                                        NilValue.INSTANCE
+                                )
+                        )
+                ),
+                // ap car ap cdr x0 ap send ap car ap cdr ap cdr x0
+                new ApplyValue(
+                        new ApplyValue(
+                                new ApplyValue(
+                                        new LazyValue() {}, // Interact
+                                        x2
+                                ),
+                                new ApplyValue(
+                                        ModemValue.INSTANCE,
+                                        new ApplyValue(
+                                                CarValue.INSTANCE,
+                                                new ApplyValue(
+                                                        CdrValue.INSTANCE,
+                                                        x0
+                                                )
+                                        )
+                                )
+                        ),
+                        new ApplyValue(
+                                SendValue.INSTANCE,
+                                new ApplyValue(
+                                        CarValue.INSTANCE,
+                                        new ApplyValue(
+                                                CdrValue.INSTANCE,
+                                                new ApplyValue(
+                                                        CdrValue.INSTANCE,
+                                                        x0
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
 
     }
 
     @Override
     public String toString() {
-        return "(f38 " + left + ")";
+        return "(f38 " + x2 + ")";
     }
 }
