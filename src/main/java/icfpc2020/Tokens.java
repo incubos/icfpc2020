07291352575a6046ab50dcfe3fb3827b30036038
Anton Volokhov <avolokhov@apple.com>
2020-07-19 19:31:30 +0100

    commands

---
+++ src/main/java/icfpc2020/Tokens.java
@@ -0,0 +1,28 @@
+package icfpc2020;
+
+import java.math.BigInteger;
+
+public class Tokens {
+    interface Token {
+    }
+
+    public static class Number implements Token {
+        public final BigInteger number;
+
+        private Number(final BigInteger number) {
+            this.number = number;
+        }
+
+        public static Number of(final long number) {
+            return new Number(BigInteger.valueOf(number));
+        }
+
+        public static Number of(final String number) {
+            return new Number(new BigInteger(number));
+        }
+
+        public static Number of(final BigInteger number) {
+            return new Number(number);
+        }
+    }
+}
