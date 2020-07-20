45a0e64cca81a0636ab08d83540ea2fcf7185fd8
Anton Volokhov <avolokhov@apple.com>
2020-07-20 11:56:33 +0100

    fix logs

--- run_local.sh
+++ run_local.sh
@@ -16,11 +16,11 @@ while read line; do
     OTHER_PLAYER_KEY=$(echo "$line" | sed -E 's/.*OTHER_PLAYER_KEY_SHOULD_BE //');
     echo "Player 2 id $OTHER_PLAYER_KEY"
     ./run.sh https://icfpc2020-api.testkontur.ru/aliens/send?apiKey=3132acdb670045d3b93482f7e0b65359 1 local $OTHER_PLAYER_KEY |\
-    tee -a games/full_2.log | grep -E '(command for shipId)|(response demList)|(Start demList)|(Join demList)' >> games/2.log; # Log only important info
+    tee -a games/full_2.log | grep -E '(Sending join request)|(Join response)|(Sending start request)|(Start response)|(Sending to server)|(Command command response)' >> games/2.log; # Log only important info
   fi;
   echo "$line" >> games/full_1.log;
   # Long only important info
-  if [[ ! -z "$(echo "$line" | grep -E '(command for shipId)|(response demList)|(Start demList)|(Join demList)')" ]]; then
+  if [[ ! -z "$(echo "$line" | grep -E '(Sending join request)|(Join response)|(Sending start request)|(Start response)|(Sending to server)|(Command command response)')" ]]; then
       echo "$line" >> games/1.log;
   fi;
 done;
